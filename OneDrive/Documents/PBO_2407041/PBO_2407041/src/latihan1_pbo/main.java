6dcf1aba0c9afc3f448f441f1b4692634adfd6c2
Mochammad Setya Pamungkas <mochammadsetya869@gmail.com>
2026-02-20 14:31:03 +0700

    PBO_2407041

---
+++ OneDrive/Documents/PBO_2407041/PBO_2407041/src/latihan1_pbo/main.java
@@ -0,0 +1,31 @@
+package latihan1_pbo;
+
+class Cafe {
+    String namaMinuman;
+    int harga;
+    String tipe;
+
+    // Constructor
+    Cafe(String namaMinuman, int harga, String tipe) {
+        this.namaMinuman = namaMinuman;
+        this.harga = harga;
+        this.tipe = tipe;
+    }
+
+    void tampilkanInfo() {
+        System.out.println("Nama Minuman : " + namaMinuman);
+        System.out.println("Harga        : Rp " + harga);
+        System.out.println("Tipe         : " + tipe);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Cafe minuman1 = new Cafe("Americano", 15000, "Panas");
+        Cafe minuman2 = new Cafe("Cappuccino", 20000, "Dingin");
+
+        minuman1.tampilkanInfo();
+        System.out.println("--------------------");
+        minuman2.tampilkanInfo();
+    }
+}
